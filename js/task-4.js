49d2a163edc0e010a64f1c0db35dced625ba447e
Maximusvin <Maximusvin@gmail.com>
2020-08-09 14:11:11 +0300

    init

---
+++ js/task-4.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const countTotalSalary = function (employees) {
+  let totalSalary = 0;
+
+  for (const key in employees) {
+    totalSalary += employees[key];
+  }
+
+  return totalSalary;
+};
+
+// Проверка
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
